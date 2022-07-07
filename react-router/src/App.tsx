3f9d864c7f92e585117516a2d98bac81ef0795e4
Arthur Sampaio <arthur.correia@holidu.com>
2022-07-07 18:47:49 +0200

    Add basic navbar

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="sticky top-0 ">
+        <nav className=" h-20 bg-slate-600  ">
+          <ul className="flex flex-row list-none m-0 p-0 odd:mx-4 items-center float-right h-full overflow-hidden  ">
+            <li className="bg-red-500 hover:bg-red-700 rounded text-sm text-white p-3">
+              Home
+            </li>
+            <li className="bg-red-500 hover:bg-red-700 rounded text-sm text-white p-3 mx-4">
+              DashBoard
+            </li>
+            <li className="bg-red-500 hover:bg-red-700 rounded text-sm text-white p-3 float-left">
+              About
+            </li>
+          </ul>
+          <h2 className="h-full flex items-center p-5 text-white text-xl">
+            Picachu
+          </h2>
+        </nav>
       </header>
+
+      <main className="mt-10 ">Main content</main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
